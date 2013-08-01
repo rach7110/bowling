cb5a6bbcc495a472b9397abcf8ca7b5fcafd8180
MakerSquare Student <students@themakersquare.com>
2013-08-01 13:37:21 -0500

    2nd test ran successfully

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,19 +1,25 @@
 class BowlingGame
 
 	def initialize 
-		@rolss = []
+		@rolls = []
 	end
 
+# Record a roll in the game.
+#
+# pins - The Integer number of pins knocked down in this roll.
+#
+# Returns nothing.
   def roll(pins)
-
+    @rolls.push(pins)
   end
 
   def score
 
   end
 
+# Returns the Integer score for this game.
   def score
-    0
+    @rolls.reduce(:+)
   end
 
 
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -13,4 +13,19 @@ describe BowlingGame do
       expect(game.score).to eq(0)
     end
   end
+
+  context 'when one pin is knocked down on each roll' do
+    it 'scores the game as 20' do
+      20.times { game.roll(1) }
+      expect(game.score).to eq(20)
+    end
+  end
 end
+# Benefits of decomposing code/refactoring:
+# more readabilty; code reuse/modularity 
+# Code Smell: >5-10 lines code for a method
+
+# Documentation driven development
+# create an empty method and then write the documentation. How would you want this method called? Give some examples. (like U/I of code writing)
+
+# Use the code before writing it. Think: how do I break this down into a series of steps?
